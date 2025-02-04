fb5af3b47b8ddbb874764029dee8d8e80c47d2d2
Andy Gong <45613281+AG2048@users.noreply.github.com>
2025-02-04 09:32:37 -0500

    Update processing_block.v

--- processing_block.v
+++ processing_block.v
@@ -5,6 +5,8 @@ module processing_block (
 )
   // Reg: have 3x3 reg, that on ready shifts all data upward by one
 
+  // Could have 9 more temp reg that stores multiplication result prior to adding, thus 1 cycle delay but improves timing
 
+  // Input tied to the bottom 3 reg, output tied to top 3 reg
 
 endmodule
