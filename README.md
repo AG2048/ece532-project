bd6bd5461b90cfce4c9e51c1272d13aa6f2714ef
AG2048 <45613281+AG2048@users.noreply.github.com>
2025-02-04 13:56:16 -0500

    processing block programming

--- README.md
+++ README.md
@@ -53,3 +53,19 @@ main:
     display_HDMI(processed_image)
     
 ```
+
+# Custom IP
+The custom IP is 2 modules working together.
+
+## Processing Block
+Input: 3 8-bit values representing one of the RGB channels of a pixel, the three values are from 3 neighboring pixels in the same row.
+
+Output: One 8-bit value representing the processed pixel value, and 3 8-bit values that are the three inputs 3 clock cycles ago.
+
+Processing Logic:
+- The input values are averaged and the result is outputted.
+- There are 3x3 internal registers that stores the values of a 3x3 grid of pixels. Every cycle, the values are pushed into the register above them, and the new values are pushed into the bottom row. The bottom row is the input to the processing block. 
+- The 3 output values are from the top row of the register.
+- Each register is also input to a multiplier block that multiplies the register value with a constant value (fixed point <8 to 20>-bit representation of 1/9). The result is stored in a register. 
+- The result of multiplication of the same row is summed together and stored in a register.
+- The sum result is summed across the 3 rows, and the output is bit-shifted back to an 8-bit representation (Due to filter being less than 1, the result will be less than max of 8-bits).
--- processing_block.v
+++ processing_block.v
@@ -1,12 +1,100 @@
-module processing_block (
+module processing_block #(
+  parameter INPUT_WIDTH = 8,
+  parameter RESULT_WIDTH = 8,
+  parameter FILTER_VALUES[3][3] = {{1/9, 1/9, 1/9}, {1/9, 1/9, 1/9}, {1/9, 1/9, 1/9}},
+  parameter FILTER_INT_BITS = 0,
+  parameter FILTER_FRACT_BITS = 8
+)
+(
+  clk, reset, enable,
   left_input, middle_input, right_input,
   left_output, middle_output, right_output,
   filter_output
 )
-  // Reg: have 3x3 reg, that on ready shifts all data upward by one
+  input clk, reset, enable;
+  input [INPUT_WIDTH-1:0] left_input, middle_input, right_input;
+  output [INPUT_WIDTH-1:0] left_output, middle_output, right_output;
+  output [RESULT_WIDTH-1:0] filter_output;
 
-  // Could have 9 more temp reg that stores multiplication result prior to adding, thus 1 cycle delay but improves timing
+  // Genvar 9 reg in a 3x3 grid, define 9 reg first with i,j index
+  reg [INPUT_WIDTH-1:0] data_reg[3][3];
+  genvar i, j;
+  generate
+    for (i = 0; i < 3; i = i + 1) begin
+      for (j = 0; j < 3; j = j + 1) begin
+        always @(posedge clk) begin
+          if (reset) begin
+            data_reg[i][j] <= 0;
+          end else begin
+            // Shift data upward by one, bottom comes from input
+            if (enable) begin
+              if (i != 2) begin
+                // Top row and middle row
+                data_reg[i][j] <= data_reg[i+1][j];
+              end else begin
+                // Bottom row
+                if (j == 0) begin
+                  data_reg[i][j] <= left_input;
+                end else if (j == 1) begin
+                  data_reg[i][j] <= middle_input;
+                end else begin
+                  data_reg[i][j] <= right_input;
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  endgenerate
 
-  // Input tied to the bottom 3 reg, output tied to top 3 reg
+  // Multiply and accumulate
+  reg [FILTER_INT_BITS+FILTER_FRACT_BITS+INPUT_WIDTH-1:0] filter_multiply_result[3][3];
+  generate
+    for (i = 0; i < 3; i = i + 1) begin
+      for (j = 0; j < 3; j = j + 1) begin
+        always @(posedge clk) begin
+          if (reset) begin
+            filter_multiply_result[i][j] <= 0;
+          end else begin
+            if (enable) begin
+              filter_multiply_result[i][j] <= data_reg[i][j] * (FILTER_VALUES[i][j] << FILTER_FRACT_BITS);
+            end
+          end
+        end
+      end
+    end
+  endgenerate
 
+  // Add all the multiplication result
+  reg [FILTER_INT_BITS+FILTER_FRACT_BITS+INPUT_WIDTH-1:0] row_accumulate_result[3];
+  reg [RESULT_WIDTH-1:0] filter_accumulate_result;
+  generate
+    for (i = 0; i < 3; i = i + 1) begin
+      always @(posedge clk) begin
+        if (reset) begin
+          row_accumulate_result <= 0;
+        end else begin
+          if (enable) begin
+            row_accumulate_result[i] <= filter_multiply_result[i][0] + filter_multiply_result[i][1] + filter_multiply_result[i][2];
+          end
+        end
+      end
+    end
+    always @(posedge clk) begin
+      if (reset) begin
+        filter_accumulate_result <= 0;
+      end else begin
+        if (enable) begin
+          filter_accumulate_result <= (row_accumulate_result[0] + row_accumulate_result[1] + row_accumulate_result[2]) >> FILTER_FRACT_BITS;
+        end
+      end
+    end
+  endgenerate
+
+  // Output
+  assign left_output = data_reg[0][0];
+  assign middle_output = data_reg[0][1];
+  assign right_output = data_reg[0][2];
+  assign filter_output = filter_accumulate_result;
 endmodule
