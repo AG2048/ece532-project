1dc1e8b183f348f678a3e1e452c9f43791bfb14d
Andy Gong <45613281+AG2048@users.noreply.github.com>
2025-02-04 09:30:57 -0500

    Create processing_block.v

---
+++ processing_block.v
@@ -0,0 +1,10 @@
+module processing_block (
+  left_input, middle_input, right_input,
+  left_output, middle_output, right_output,
+  filter_output
+)
+  // Reg: have 3x3 reg, that on ready shifts all data upward by one
+
+
+
+endmodule
